6f6d172efdfb1291a7252e22219dd7d2a05f0487
creuzige <creuzige@nist.gov>
2020-07-10 13:32:45 -0400

    Added plots for Single Texture Schemes

--- JupyterNotebooks/PlotTexture.py
+++ JupyterNotebooks/PlotTexture.py
@@ -29,3 +29,44 @@ def SimpleTestPlot(Name, Coordinates, save=False,cmd=False,savename='test.png'):
 
     if cmd==False:
         plt.show()
+
+
+#####################################
+# Plot a single sampling scheme
+#####################################
+def SingleSchemePlot(Name, Coordinates, Marker, Markersize, save=False, cmd=False, savename='test.png'):
+    """
+    Test plot to work out conventions
+    """
+    import pandas as pd
+    import mplstereonet
+    from matplotlib import pyplot as plt
+    
+    fig = plt.figure(figsize=(8,9), dpi=600)
+
+
+    #key
+    ax1 = fig.add_subplot(111, projection='stereonet')
+    ax1.set_azimuth_ticks([0,90], labels=['RD','-- TD'],fontsize=14)
+    ax1.plane(0.0, 90.0, 'k-', linewidth=1)
+    ax1.plane(90.0, 90.0, 'k-', linewidth=1)
+    ax1.annotate('ND', xy=(0, 0), xytext=(0.1,0.1), fontsize=14)
+        
+    
+    #SingleOrientation - Name, Tilt, Rotation
+    #SchemeName,Coordinates=SingleOrientation("RD Single", 90.0,180.0)
+    #SchemeName,Coordinates=SingleOrientation("TD Single", 90.0,270.0)
+
+    dip, strike =Coordinates['Tilt'], Coordinates['Rotation']-90.0
+    l1=ax1.pole(strike, dip, Marker, markersize=10, clip_on=False)
+    #dip tilts about the 0 axis (RD), left handed
+    #strike tilts about the normal axis (ND), left handed
+    #Looking at the bottom of the sphere, not the top
+    #this is just convention of mplstereonet, does not affect averaging methods
+
+    if save==True:
+        plt.savefig(savename, bbox_inches='tight')
+
+    if cmd==False:
+        plt.show()
+
